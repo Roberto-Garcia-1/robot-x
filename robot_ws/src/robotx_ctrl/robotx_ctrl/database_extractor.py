129185ffb3049455c17b7f0b6a956a2276f8207f
Roberto-Garcia-1 <garg.roberto@gmail.com>
2025-10-12 23:52:56 -0600

    Added database tools

---
+++ robot_ws/src/robotx_ctrl/robotx_ctrl/database_extractor.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import sqlite3
+import rosbag2_py
+from rclpy.serialization import deserialize_message
+from std_msgs.msg import Float32MultiArray
+from sensor_msgs.msg import Image
+import os
+from cv_bridge import CvBridge
+import cv2
+from datetime import datetime
+bridge = CvBridge()
+
+# --- Open rosbag ---
+bag_path=os.path.expanduser("~/ROS2Dev/ros2bag/ros2bagcamnew")
+# --- Crear base SQLite de salida ---
+db_path = os.path.expanduser("~/ROS2Dev/ros2bag/mis_datos.db")
+conn = sqlite3.connect(db_path)
+# Carpeta para imagenes
+os.makedirs("imagenes", exist_ok=True)
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS floats (t REAL, d1 REAL, d2 REAL, d3 REAL, d4 REAL, d5 REAL, d6 REAL)")
+cursor.execute("CREATE TABLE IF NOT EXISTS imagenes (t_ns INTEGER, timestamp_text TEXT, data BLOB)")
+img_count = 0
+
+storage_options = rosbag2_py.StorageOptions(
+    uri=bag_path,  # carpeta donde está el bag
+    storage_id="sqlite3"
+)
+converter_options = rosbag2_py.ConverterOptions("", "")
+reader = rosbag2_py.SequentialReader()
+reader.open(storage_options, converter_options)
+
+# --- Leer mensajes ---
+
+while reader.has_next():
+  (topic, data, t) = reader.read_next()
+  t_sec = t * 1e-9
+  timestamp_text = datetime.fromtimestamp(t_sec).strftime("%Y-%m-%d %H:%M:%S.%f")
+  if topic == "/new_array":
+    msg = deserialize_message(data, Float32MultiArray)
+    print(len(msg.data))
+    cursor.execute("INSERT INTO floats VALUES (?, ?, ?, ?, ?, ?, ?)", (t, *msg.data))
+  elif topic == "/camera/image_raw":
+    print("image found")
+    msg = deserialize_message(data, Image)
+
+    # Convertir a imagen OpenCV con cv_bridge
+    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+
+    # Redimensionar a 640x480
+    resized = cv2.resize(cv_image, (640, 480))
+
+    # Guardar como archivo JPG
+    filename = f"imagenes/img_{img_count:05d}.jpg"
+    cv2.imwrite(filename, resized)
+    with open(filename, "rb") as f:
+      img_blob = f.read()
+      cursor.execute("INSERT INTO imagenes VALUES (?, ?, ?)", (t, timestamp_text, img_blob))
+
+conn.commit()
+conn.close()
---
+++ robot_ws/src/robotx_ctrl/robotx_ctrl/rosbag_recorder.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+import os
+import select
+import sys
+
+import rclpy
+from rclpy.node import Node
+from rclpy.serialization import serialize_message
+from sensor_msgs.msg import Image
+from robotx_interfaces.msg import RobotFrame 
+from rosbag2_py import SequentialWriter, StorageOptions, ConverterOptions, TopicMetadata
+from datetime import datetime
+import threading
+from pynput import keyboard
+import time
+import shutil
+import termios
+import tty
+class BagRecorder(Node):
+    def __init__(self):
+        super().__init__('bag_recorder')
+        # Suscripciones
+        self.image_msg = None
+        self.robot_frame_msg = None
+        self.create_subscription(Image, '/camera/image_raw', self.image_callback, 10)
+        self.create_subscription(RobotFrame, '/robot_frame', self.robot_frame_callback, 10)
+
+        # Estado
+        self.recording = False
+        self.writer = None
+        self.last_folder = None
+
+        self.timer = self.create_timer(0.1, self.record_messages)
+
+        # Hilos
+        """self.keyboard_thread = threading.Thread(target=self.start_keyboard_listener, daemon=True)
+        self.keyboard_thread.start()"""
+
+        self.get_logger().info("Nodo iniciado. [Espacio]=Iniciar/detener grabación, [x]=Borrar última carpeta.")
+
+    # --- Callbacks de tópicos ---
+    def image_callback(self, msg):
+        self.image_msg = msg
+
+    def robot_frame_callback(self, msg):
+        self.robot_frame_msg = msg
+
+    # --- Control con teclado ---
+    def start_keyboard_listener(self):
+        print("dsadsa")
+        def on_press(key):
+            print("asdsda")
+            try:
+                if key == keyboard.Key.space:
+                    self.toggle_recording()
+                elif hasattr(key, 'char') and key.char == 'x':
+                    self.delete_last_folder()
+            except AttributeError:
+                print(f'Special key pressed: {key}')
+
+        listener = keyboard.Listener(on_press=on_press)
+        listener.start()
+
+
+    def get_key(self, settings):
+      tty.setraw(sys.stdin.fileno())
+      rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+      if rlist:
+          key = sys.stdin.read(1)
+      else:
+          key = ''
+      termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+      return key
+    
+
+    # --- Grabación ---
+    def toggle_recording(self):
+        if not self.recording:
+            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            folder_name = f"rosbags/rosbag_{timestamp}"
+            #os.makedirs(folder_name, exist_ok=True)
+            self.last_folder = folder_name
+
+            
+            
+            self.writer = SequentialWriter()
+            storage_options = StorageOptions(
+                uri=folder_name,
+                storage_id='sqlite3'
+            )
+            converter_options = ConverterOptions('', '')
+            self.writer.open(storage_options, converter_options)
+            
+            """self.writer.create_topic({
+                'name': '/camera/image_raw',
+                'type': 'sensor_msgs/msg/Image',
+                'serialization_format': 'cdr'
+            })
+            self.writer.create_topic({
+                'name': '/robot_frame',
+                'type': 'your_package_name/msg/RobotFrame',  # cambia aquí
+                'serialization_format': 'cdr'
+            })"""
+
+            topic_info = TopicMetadata(
+            name='/camera/image_raw',
+            type='sensor_msgs/msg/Image',
+            serialization_format='cdr')
+            self.writer.create_topic(topic_info)
+            topic_info = TopicMetadata(
+            name='/robot_frame',
+            type='RobotFrame/msg/RobotFrame',
+            serialization_format='cdr')
+            self.writer.create_topic(topic_info)
+
+            self.recording = True
+            self.get_logger().info(f"Grabando a 10Hz en carpeta: {folder_name}")
+        else:
+            self.recording = False
+            self.writer = None
+            self.get_logger().info("Grabación detenida. Presiona [Espacio] para iniciar otra o [x] para eliminar la última carpeta.")
+
+    def record_messages(self):
+        """Se ejecuta a 10Hz"""
+        #Verifica teclado
+        key = self.get_key(termios.tcgetattr(sys.stdin))
+        if key == " ":
+            self.toggle_recording()
+        elif key == 'x':
+            self.delete_last_folder()
+
+        if not self.recording or self.writer is None:
+            return
+
+        now = self.get_clock().now().to_msg()
+        timestamp = int(self.get_clock().now().nanoseconds)
+
+        if self.image_msg is not None:
+            self.writer.write(
+                '/camera/image_raw',
+                serialize_message(self.image_msg),
+                timestamp
+            )
+
+        if self.robot_frame_msg is not None:
+            self.writer.write(
+                '/robot_frame',
+                serialize_message(self.robot_frame_msg),
+                timestamp
+            )
+
+    # --- Eliminar última carpeta ---
+    def delete_last_folder(self):
+        if not self.last_folder:
+            self.get_logger().warn("No hay carpeta anterior para borrar.")
+            return
+
+        confirm = input(f"¿Seguro que deseas borrar '{self.last_folder}'? (s/n): ")
+        if confirm.lower() == 's':
+            shutil.rmtree(self.last_folder, ignore_errors=True)
+            self.get_logger().info(f"Carpeta '{self.last_folder}' borrada.")
+            self.last_folder = None
+        else:
+            self.get_logger().info("Operación cancelada.")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = BagRecorder()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
